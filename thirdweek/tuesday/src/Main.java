1dc4f7e20f8eec6de64558e23207bafc90280786
nurfad7 <nurfad7@gmail.com>
2024-09-17 15:01:46 +0700

    feat: add main of tuesday exercise third-week

---
+++ thirdweek/tuesday/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
